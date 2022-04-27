c28c35aa449a4d8dfb9344e366cf7c6dfc3f665f
kocirfan <irfnk83@gmail.com>
2022-04-27 23:20:30 +0300

    state yapısı

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -4,6 +4,27 @@ import React from "react";
 
 class UserSignupPage extends React.Component{
 
+    //ovveride metot state
+    state = {
+        username: null,
+        displayName: null,
+        password: null,
+        passwordRepeat : null
+    }
+                     
+    onChange = event => {
+         const {name, value} = event.target; //object destruction
+         
+            //event.target.value forma girilen değeri tutuyor
+            // event.target.name formda ilgili alanı tutuyor
+        
+         this.setState({
+            [name]: value    //[name]: value name ile value set ediliyor
+        });
+    }
+
+    
+
     //ovveride ettiği metot/ class componentlerde render zorunlu
     render(){
         // render metodu bir jsx dönmelidir
@@ -12,19 +33,19 @@ class UserSignupPage extends React.Component{
                 <h1>Sign Up</h1>
                 <div>
                     <label>Username</label>
-                    <input />
+                    <input name="username" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input />
+                    <input name="displayName" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" />
+                    <input name="password" type="password"  onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password" />
+                    <input name="passwordRepeat" type="password" onChange={this.onChange} />
                 </div>
                 <button>Sign Up</button>
             </form>    
