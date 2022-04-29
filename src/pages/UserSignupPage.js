69e541a27ced0b292966727b1c039bdf2c97d66e
kocirfan <irfnk83@gmail.com>
2022-04-29 04:22:55 +0300

    validation ve custom component

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {signup} from "../api/apiCalls"
+import {signup} from "../api/apiCalls";
+import Input from "../components/Input";
 
 //bir class Component- statefull
 
@@ -10,17 +11,30 @@ class UserSignupPage extends React.Component {
     displayName: null,
     password: null,
     passwordRepeat: null,
-    pendingApiCall: false
+    pendingApiCall: false,
+    errors: {}
   };
 
   onChange = (event) => {
     const { name, value } = event.target; //object destruction
+    const errors = {...this.state.errors} // errors objesinin kopyasını oluşturmak ...
+    errors[name] = undefined;
+    if(name === 'password' || name === 'passwordRepeat'){
+      if(name === 'password' && value !== this.state.passwordRepeat){
+        errors.passwordRepeat = 'Password mismatch';
+      }else if(name === 'passwordRepeat' && value !== this.state.password){
+        errors.passwordRepeat = 'Password mismatctc';
+      }else{
+        errors.passwordRepeat = undefined;
+      }
+    }
 
     //event.target.value forma girilen değeri tutuyor
     // event.target.name formda ilgili alanı tutuyor
 
     this.setState({
-      [name]: value //[name]: value name ile value set ediliyor
+      [name]: value, //[name]: value name ile value set ediliyor
+      errors
     });
   };
 
@@ -39,7 +53,11 @@ class UserSignupPage extends React.Component {
 
     try{
         const response = await signup(body);
-    }catch(error){}
+    }catch(error){
+      if(error.response.data.validationErrors){
+        this.setState({errors: error.response.data.validationErrors});
+      }
+    }
 
     this.setState({ pendingApiCall: false});
     // aktif sorgu olduğu durumda tekrarlayan işlemi önlemk adına axios da durum yönetimi
@@ -47,37 +65,23 @@ class UserSignupPage extends React.Component {
 
   //ovveride ettiği metot/ class componentlerde render zorunlu
   render() {
-    const {pendingApiCall} = this.state;
+    const {pendingApiCall, errors} = this.state;
+    const {username, displayName, password, passwordRepeat} = errors;
     // render metodu bir jsx dönmelidir
     return (
       <div className="container">
         <form>
           <h1 className="text-center">Sign Up</h1>
-          <div className="form-group">
-            <label>Username</label>
-            <input className="form-control" name="username" onChange={this.onChange} />
-          </div>
-          <div className="form-group">
-            <label>Display Name</label>
-            <input className="form-control" name="displayName" onChange={this.onChange} />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input className="form-control" name="password" type="password" onChange={this.onChange} />
-          </div>
-          <div className="form-group">
-            <label>Password Repeat</label>
-            <input className="form-control"
-              name="passwordRepeat"
-              type="password"
-              onChange={this.onChange}
-            />
-          </div>
+          <Input name="username" label="Username" error={username} onChange={this.onChange} />
+          <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange} />
+          <Input name="password" label="Password" error={password} type="password" onChange={this.onChange} />
+          <Input name="passwordRepeat" label="password Repeat" error={passwordRepeat} type="password" onChange={this.onChange} />
+         
           <div className="text-center">
             <button 
              className="btn btn-primary mt-2"
              onClick={this.onClickSignup}
-             disabled={pendingApiCall}>
+             disabled={pendingApiCall || passwordRepeat !== undefined}>
                  {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>}
                  Sign Up
             </button>
